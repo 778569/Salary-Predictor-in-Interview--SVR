8d704a1cb77b5ded844440cfb099aab9bf158cfa
Kavinda Bandara <52319671+778569@users.noreply.github.com>
2024-07-22 09:47:06 +0530

    Add files via upload

---
+++ SMV_Flask_API.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+import sklearn
+
+app = Flask(__name__)
+
+model = joblib.load('SVM_Salary.pkl')
+sc_X = joblib.load('sc_X.pkl')
+sc_Y = joblib.load('sc_Y.pkl')
+
+@app.route('/')
+
+def home():
+    return "Welcome to the SVR Model Prediction API!"
+
+@app.route('/predict', methods=['POST'])
+
+def predict():
+    # Get the data from the POST request
+    data = request.json
+    print("Received data:", data)
+    features = np.array([[data['feature']]])
+
+    # Preprocess the data
+    features_transformed = sc_X.transform(features)
+ # Make prediction
+    prediction = model.predict(features_transformed)
+
+    # Inverse transform to original scale
+    prediction_original_scale = sc_Y.inverse_transform(prediction.reshape(-1, 1))
+
+    # Return the result as a JSON response
+    return jsonify({'prediction': prediction_original_scale[0, 0]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
